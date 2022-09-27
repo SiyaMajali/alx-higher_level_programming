81424eadc114ccbdbae3ff5c7417081b83b8b269
Majali D Siyamthanda <74431717+SiyaMajali@users.noreply.github.com>
2022-09-27 04:24:19 +0200

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_list = list(my_string)
-    for letter in my_list:
-        if letter == "c":
-            my_list.remove("c")
-        elif letter == "C":
-            my_list.remove("C")
-    new_string = ""
-    new_string = new_string.join(my_list)
-    return new_string
+    new = []
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new.append(i)
+    return ''.join(new)
