865d645491c5839267512a68b620ef0fb9103bae
Majali D Siyamthanda <74431717+SiyaMajali@users.noreply.github.com>
2022-09-29 00:41:02 +0200

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+def convert(letter):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for key in roman:
+        if letter == key:
+            return roman[key]
+
+
+def roman_to_int(roman_string):
+
+    if roman_string is None:
+        return 0
+
+    if not isinstance(roman_string, str):
+        return 0
+    sum = 0
+    prev = 0
+    for idx in range(0, len(roman_string)):
+        val1, val2 = 0, 0
+        val1 = convert(roman_string[idx])
+        if prev == 0:
+            if idx + 1 < len(roman_string):
+                val2 = convert(roman_string[idx + 1])
+                if val2 > val1:
+                    prev = val1
+                else:
+                    sum = sum + val1
+            else:
+                sum = sum + val1
+                break
+
+        else:
+            sum = sum + val1 - prev
+            prev = 0
+
+    return sum
