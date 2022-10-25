d321f32fd8a701a667b56ee2cadb28feee518ba2
Majali D Siyamthanda <74431717+SiyaMajali@users.noreply.github.com>
2022-10-25 04:14:04 +0200

    100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+class MyInt
+inherits from int
+"""
+
+
+class MyInt(int):
+    def __eq__(self, other):
+        """
+        equal to
+        """
+        return False
+
+    def __ne__(self, other):
+        """
+        not equal to
+        """
+        return True
