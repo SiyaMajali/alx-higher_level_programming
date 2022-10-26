52862bb8de3ceecc02e6871998d949ba73cc36b6
Majali D Siyamthanda <74431717+SiyaMajali@users.noreply.github.com>
2022-10-26 03:45:08 +0200

    3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""
+contains to_json_string function
+"""
 
-"""
-function that
-returns JSON rep of obj
-"""
+
 import json
 
 
 def to_json_string(my_obj):
+    """
+    function return the json repersention of an objecrt(string)
+    """
     return json.dumps(my_obj)
