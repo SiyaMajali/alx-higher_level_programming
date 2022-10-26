b35c663856c94cf604ea570022a21fe228a76445
Majali D Siyamthanda <74431717+SiyaMajali@users.noreply.github.com>
2022-10-26 04:06:47 +0200

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""Module for saving to json"""
-import json
-import os.path
-import sys
-from sys import argv
+"""Load, add, save module"""
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+
+from sys import argv
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
 filename = "add_item.json"
-json_list = []
 
-if os.path.exists(filename):
-    json_list = load_from_json_file(filename)
-
-for index in argv[1:]:
-    json_list.append(index)
-
-save_to_json_file(json_list, filename)
+try:
+    my_list = load_from_json_file(filename)
+except:
+    my_list = []
+for a in range(1, len(argv)):
+    my_list.append(argv[a])
+save_to_json_file(my_list, filename)
