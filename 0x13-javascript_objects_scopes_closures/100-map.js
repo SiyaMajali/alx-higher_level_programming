891c480693179ba803d405788dd17e1dce8399a4
Majali D Siyamthanda <74431717+SiyaMajali@users.noreply.github.com>
2022-12-21 03:57:29 +0200

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
-console.log(list);
-console.log(list.map((items, index) => items * index));
+const lists = require('./100-data').list;
+console.log(lists);
+const newList = lists.map((items, index) => items * index);
 console.log(newList);
