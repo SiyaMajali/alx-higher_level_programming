6786a82816bfb09c7a21a8db68e0b62de08a70d6
Majali D Siyamthanda <74431717+SiyaMajali@users.noreply.github.com>
2022-12-21 03:42:24 +0200

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-// maps an array, multiplying each element by its index
+
 const list = require('./100-data').list;
+
 console.log(list);
-console.log(list.map((element, index) => element * index));
+console.log(list.map((x, i) => x * i));
