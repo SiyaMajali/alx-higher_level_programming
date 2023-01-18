82c9f021999dbccfd06114166e588d89ccd8620f
Majali D Siyamthanda <74431717+SiyaMajali@users.noreply.github.com>
2023-01-18 02:31:00 +0200

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 """
-script to fetch header
+takes in a URL, sends a request to the URL and displays the value of the
+X-Request-Id variable found in the header of the response
 """
-
-
-if __name__ == '__main__':
-    from urllib.request import Request, urlopen
+if __name__ == "__main__":
+    import urllib.request as request
     from sys import argv
-
-    req = Request(argv[1])
-    with urlopen(req) as res:
-        header = res.info()
-        print(header['X-Request-Id'])
+    req = request.Request(argv[1])
+    with request.urlopen(req) as r:
+        print(r.headers.get('X-Request-Id'))
