d37f63b71c18c84db92f0f65ffffac88ccc76620
Majali D Siyamthanda <74431717+SiyaMajali@users.noreply.github.com>
2023-01-18 02:25:53 +0200

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,23 +1,16 @@
+
 #!/usr/bin/python3
-"""
-fetches https://alx-intranet.hbtn.io/status
-"""
-     import urllib.request as request
+"""fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
 
 
 if __name__ == "__main__":
-     """fetches https://alx-ntranet.hbtn.io/status"""
-   
-     
-     import urllib.request as request
-   
-     
-     with request.urlopen("https://alx-intranet.hbtn.io/status") as r:
-        html = r.read()
-      html_str = html.decode("utf-8")
-        
-     
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-print("\t- utf8 content: {}".format(html_str))
+    """fetches https://alx-intranet.hbtn.io/status"""
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        html_str = html.decode('utf-8')
+
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html_str))
