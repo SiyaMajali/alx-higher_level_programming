4ea35fda860b9a273d7c9e167ce20687a019118f
Majali D Siyamthanda <74431717+SiyaMajali@users.noreply.github.com>
2023-01-18 02:20:16 +0200

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,11 @@
 fetches https://alx-intranet.hbtn.io/status
 """
 if __name__ == "__main__":
-     """fetches https://intranet.hbtn.io/status"""
+     """fetches https://ialx-ntranet.hbtn.io/status"""
     import urllib.request as request
-    with request.urlopen('https://alx-intranet.hbtn.io/status') as r:
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as r:
         html = r.read()
-        print('Body response:')
+        print("Body response:")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
