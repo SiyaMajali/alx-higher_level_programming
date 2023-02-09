55dd198e794e1c577b62036c0f87f2ec2c43dd11
Majali D Siyamthanda <74431717+SiyaMajali@users.noreply.github.com>
2023-02-10 00:44:40 +0200

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-const fs = require('fs');
-const file = process.argv[2];
-fs.readFile(file, 'utf-8', function (err, data) {
+// Reads and prints the content of a file
+
+const argv = process.argv;
+let fs = require('fs');
+fs.readFile(argv[2], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
